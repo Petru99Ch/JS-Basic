186fa35d8688c693b675bcb5232902e8ab60db22
Petru99Ch <117940325+Petru99Ch@users.noreply.github.com>
2023-02-25 20:28:33 +0200

    Add files via upload

---
+++ Calculations-age-payment/calc-age.js
@@ -0,0 +1,17 @@
+//HW1: what role does "const" prefix play ? 
+//"const" is a variable that cannot be changed. (unchanging variable)
+
+
+//HW2:
+
+//calculate Payment a day
+
+//data
+let hourPay = 10;
+let hourWorking = prompt("how hours you works a day")
+
+//calculations
+
+let paymentDay = hourWorking * hourPay
+
+alert(paymentDay)
