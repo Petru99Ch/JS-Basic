3ce4f81483fd212c5ef1dc3c96d74e226278cf32
Petru99Ch <117940325+Petru99Ch@users.noreply.github.com>
2023-02-25 20:34:29 +0200

    Add files via upload

---
+++ calc-age.js
@@ -0,0 +1,12 @@
+//calculates the person's age
+
+//data / variables
+
+let currentYear = new Date().getFullYear()
+let birthYear = prompt("enter your year of birth")
+
+//calculations
+let age = currentYear - birthYear
+
+//show
+alert(age)
