390a144a57223a8757ad08ec3c5678a11d11862a
Petru99Ch <117940325+Petru99Ch@users.noreply.github.com>
2023-05-09 13:25:30 +0300

    Add files via upload

---
+++ Object-Basic/object.js
@@ -0,0 +1,29 @@
+// HW : 
+// choose a book, film, product, 
+// try to structure the information about them using literal objects 
+
+let car_BMW = {
+    model_series: 5,
+    capacity_engine: "3.0",
+    type_fuel: "diesel",
+    transmision: "full",
+    color: "black",
+    air_condition: true,   
+}
+
+let film_FastFurious = {
+    createdBy : "Garry Scott Thompson",
+    years: "2001 - present",
+    genre: "action",
+    actor: ["Paul Walker", "Vin Diesel", "Michelle Rodriguez", "Tyrese Gibson"]
+}
+
+let iPhone = {
+    battery_capacity: 3279,
+    memory: 128,
+    ram : 8,
+    face_id: true , 
+    audio_system: "stereo",
+    os : "iOS",
+    vBluetooth: "v5.3"
+}
